bd70faf5df6383441fbd622c88c94e3ed03d8867
alexandra <alexandra.ding.mm@gmail.com>
2025-01-17 23:41:24 -0500

    started elevator code
    created elevator class with 2 kraken motors + command that sets position of elevator and button binded the command
    created elevator subclass in constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,9 +2,37 @@ package frc.robot;
 
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 
+//numbers are dummies
 public class Constants {
     /* general */
     public static boolean kDebugLoggingEnabled = true;
+
+    public class Elevatork{
+        public static final int kLeftCANID = 10;
+        public static final int kRightCANID = 11;
+
+        public static final double kGearRatio = 0.2;
+        public static final double kSpoolDiameter = 5.5;
+
+        private static final CurrentLimitsConfigs kLimitConfigs = new CurrentLimitsConfigs()
+            .withStatorCurrentLimit(100)
+            .withSupplyCurrentLimit(50)
+            .withStatorCurrentLimitEnable(true);
+        private static final FeedbackConfigs kFeedbackConfigs = new FeedbackConfigs();
+        private static final MotionMagicConfigs kMagicConfigs = new MotionMagicConfigs();
+
+        public static final TalonFXConfiguration kLeftTalonFXConfiguration = new TalonFXConfiguration()
+            .withCurrentLimits(kLimitConfigs)
+            .withFeedback(kFeedbackConfigs)
+            .withMotionMagic(kMagicConfigs);
+        public static final TalonFXConfiguration kRightTalonFXConfiguration = new TalonFXConfiguration()
+            .withCurrentLimits(kLimitConfigs)
+            .withFeedback(kFeedbackConfigs)
+            .withMotionMagic(kMagicConfigs);
+
+    }
 }
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,1 +1,49 @@
-/* left as an exercise for the reader */
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicExpoVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants.Elevatork;
+import frc.robot.generated.TunerConstants;
+
+//numbers are dummies
+public class Elevator{
+    private final TalonFX m_left = new TalonFX(Elevatork.kLeftCANID, TunerConstants.kCANBus);
+	private final TalonFX m_right = new TalonFX(Elevatork.kRightCANID, TunerConstants.kCANBus);
+    private final Follower m_follower = new Follower(m_right.getDeviceID(),true);
+    private final MotionMagicExpoVoltage m_MMEVRequest = new MotionMagicExpoVoltage(0);
+
+    public Elevator(){
+        m_left.setControl(m_follower);
+        m_left.getConfigurator();
+        m_right.getConfigurator();
+
+    }
+
+    public Command setPosition(double heightMeters){
+        return Commands.runOnce(() -> m_right.setControl(m_MMEVRequest.withPosition(heightMeters)));
+    }
+
+    public Command setPosition(HeightPosition heightMeters){
+        return setPosition(heightMeters.m_heightMeters);
+    }
+
+    public enum HeightPosition{
+        HOME(2),
+        L1(5),
+        L2(6),
+        L3(8),
+        L4(10);
+
+        public final double m_heightMeters;
+
+        private HeightPosition(double heightMeters){
+            m_heightMeters = heightMeters;
+        }
+
+    }
+
+}
