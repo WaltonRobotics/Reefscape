cc5f7da7c39c6b3c664a79f4b4d06a42567adb7d
Banks Troutman <btrout.dhrs@gmail.com>
2025-01-19 12:50:23 -0500

    Remove robotcontainer

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,17 +4,98 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.*;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import choreo.auto.AutoChooser;
+import choreo.auto.AutoFactory;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.autons.WaltAutonFactory;
+import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Swerve;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
-  private final RobotContainer m_robotContainer;
+  private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+  private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+
+  /* Setting up bindings for necessary control of the swerve drive platform */
+  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+    .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+    .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+
+  private final Telemetry logger = new Telemetry(MaxSpeed);
+
+  private final CommandXboxController joystick = new CommandXboxController(0);
+
+  public final Swerve drivetrain = TunerConstants.createDrivetrain();
+
+  public final Elevator elevator = new Elevator();
+
+  private final AutoFactory autoFactory = drivetrain.createAutoFactory();
+  private final WaltAutonFactory waltAutonFactory = new WaltAutonFactory(autoFactory);
+  private final AutoChooser autoChooser = new AutoChooser();
 
   public Robot() {
-    m_robotContainer = new RobotContainer();
+    /* autossss */
+    // autoChooser.addRoutine("ezTest1", waltAutonFactory::ezTest1);
+    // autoChooser.addRoutine("middle_2pc", waltAutonFactory::middle_2pc);
+
+    SmartDashboard.putData("Auto Chooser", autoChooser);
+
+    configureBindings();
+  }
+
+  private void configureBindings() {
+    // Note that X is defined as forward according to WPILib convention,
+    // and Y is defined as to the left according to WPILib convention.
+    drivetrain.setDefaultCommand(
+      // Drivetrain will execute this command periodically
+      drivetrain.applyRequest(() ->
+        drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+          .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+          .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+      )
+    );
+
+    joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+    joystick.b().whileTrue(drivetrain.applyRequest(() ->
+      point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
+    ));
+
+    // Run SysId routines when holding back/start and X/Y.
+    // Note that each routine should be run exactly once in a single log.
+    //joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+    //joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+    //joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+    //joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+
+    // reset the field-centric heading on left bumper press
+    joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+
+    //joystick.rightBumper().whileTrue(drivetrain.wheelRadiusCharacterization(1));
+    //joystick.rightTrigger().whileTrue(drivetrain.wheelRadiusCharacterization(-1));
+
+    joystick.leftTrigger().whileTrue(elevator.setPosition(Elevator.HeightPosition.L2));
+    joystick.rightBumper().and(joystick.leftBumper())
+      .whileTrue(elevator.setPosition(Elevator.HeightPosition.L1));
+    joystick.leftTrigger().and(joystick.rightTrigger())
+      .whileTrue(elevator.setPosition(Elevator.HeightPosition.L3));
+    joystick.x().whileTrue(elevator.setPosition(Elevator.HeightPosition.L4));
+    joystick.y().whileTrue(elevator.setPosition(Elevator.HeightPosition.HOME));
+
+    drivetrain.registerTelemetry(logger::telemeterize);
   }
 
   @Override
@@ -33,7 +114,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    m_autonomousCommand = autoChooser.selectedCommand();
 
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
