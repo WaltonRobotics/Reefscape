003c9372307e57e7382d220e80f5eae418bfefee
scootdboot <andersonscott100@gmail.com>
2025-01-23 08:50:36 -0500

    Units Library

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,18 +1,20 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Kilograms;
+import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Second;
 
-import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.Mass;
 
 //numbers are dummies
 public class Constants {
@@ -24,19 +26,20 @@ public class Constants {
         public static final int kRightCANID = 11;
 
         public static final double kGearRatio = 27;
-        public static final double kSpoolRadius = 1; //inches
+        public static final Distance kSpoolRadius = Inches.of(1);
 
-        public static final double kCarriageMassKg = 4;
-        public static final double kMaximumHeight = 6; //meters
-        public static final double kStartingHeightMeters = 0; //meters
-        public static final double kSensorToMechanismRatio = (kGearRatio) / (2 * Units.inchesToMeters(kSpoolRadius) * Math.PI);
+        public static final Mass kCarriageMassKg = Kilograms.of(4);
+        public static final Distance kMinimumHeight = Meters.of(0);
+        public static final Distance kMaximumHeight = Meters.of(6);
+        public static final Distance kStartingHeightMeters = Meters.of(0);
+        public static final double kSensorToMechanismRatio = (kGearRatio) / (kSpoolRadius.in(Meters) * Math.PI);
 
         private static final CurrentLimitsConfigs kLimitConfigs = new CurrentLimitsConfigs()
             .withStatorCurrentLimit(100)
             .withSupplyCurrentLimit(50)
             .withStatorCurrentLimitEnable(true);
         private static final FeedbackConfigs kFeedbackConfigs = new FeedbackConfigs()
-            .withSensorToMechanismRatio((kGearRatio) / (Units.inchesToMeters(kSpoolRadius) * Math.PI));
+            .withSensorToMechanismRatio((kGearRatio) / (kSpoolRadius.in(Meters) * Math.PI));
         // all of these dummy values are stolen from a ctre example project
         private static final MotionMagicConfigs kMagicConfigs = new MotionMagicConfigs()
             .withMotionMagicCruiseVelocity(RotationsPerSecond.of(5/10))
