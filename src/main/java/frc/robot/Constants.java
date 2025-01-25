5e3a8cf0dd6a3a3ec883c88d5b904c22b876a93f
graciexie <xiegrace61@gmail.com>
2025-01-25 10:22:43 -0500

    lol oops mb guys algaek is back now

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,6 +16,66 @@ public class Constants {
     /* general */
     public static boolean kDebugLoggingEnabled = true;
 
+     // TODO: NONE OF THESE ARE REAL NUMBERS!!!!!!!!!!!!!!!!!
+    // BE WARY OF MOTOR NUMBERS - MANY ARE STOLEN FROM ELEVATOR!!!!! (couldn't easily find better ones)
+    public static class AlgaeK {
+        public static final int kWristCANID = 12;
+        public static final int kIntakeCANID = 13;
+
+        // motor configuration section
+        // wrist motor
+        public static final int kWristGearRatio = 10;
+        public static final int kWristSensorToMechanismRatio = kWristGearRatio;
+        private static final CurrentLimitsConfigs kWristCurrentLimitConfigs = new CurrentLimitsConfigs()
+            .withStatorCurrentLimit(100)
+            .withSupplyCurrentLimit(50)
+            .withStatorCurrentLimitEnable(true);
+        private static final FeedbackConfigs kWristFeedbackConfigs = new FeedbackConfigs()
+            .withSensorToMechanismRatio(kWristSensorToMechanismRatio);
+        private static final MotionMagicConfigs kWristMagicConfigs = new MotionMagicConfigs()
+            .withMotionMagicCruiseVelocity(RotationsPerSecond.of(1))
+            .withMotionMagicAcceleration(RotationsPerSecondPerSecond.of(10))
+            .withMotionMagicJerk(RotationsPerSecondPerSecond.per(Second).of(100));
+        private static final Slot0Configs kWristSlot0Configs = new Slot0Configs()
+            .withKS(0.25)
+            .withKV(0.12)
+            .withKA(0.01)
+            .withKP(60)
+            .withKI(0)
+            .withKD(0.5);
+        public static final TalonFXConfiguration kWristConfiguration = new TalonFXConfiguration()
+            .withCurrentLimits(kWristCurrentLimitConfigs)
+            .withFeedback(kWristFeedbackConfigs)
+            .withMotionMagic(kWristMagicConfigs)
+            .withSlot0(kWristSlot0Configs);
+
+        // intake motor
+        public static final int kIntakeGearRatio = 10;
+        public static final int kIntakeSensorToMechanismRatio = kIntakeGearRatio;
+        private static final CurrentLimitsConfigs kIntakeCurrentLimitConfigs = new CurrentLimitsConfigs()
+            .withStatorCurrentLimit(100)
+            .withSupplyCurrentLimit(50)
+            .withStatorCurrentLimitEnable(true);
+        private static final FeedbackConfigs kIntakeFeedbackConfigs = new FeedbackConfigs()
+            .withSensorToMechanismRatio(kIntakeSensorToMechanismRatio);
+        private static final MotionMagicConfigs kIntakeMagicConfigs = new MotionMagicConfigs()
+            .withMotionMagicCruiseVelocity(RotationsPerSecond.of(1))
+            .withMotionMagicAcceleration(RotationsPerSecondPerSecond.of(10))
+            .withMotionMagicJerk(RotationsPerSecondPerSecond.per(Second).of(100));
+        private static final Slot0Configs kIntakeSlot0Configs = new Slot0Configs()
+            .withKS(0.25)
+            .withKV(0.12)
+            .withKA(0.01)
+            .withKP(60)
+            .withKI(0)
+            .withKD(0.5);
+        public static final TalonFXConfiguration kIntakeConfiguration = new TalonFXConfiguration()
+            .withCurrentLimits(kIntakeCurrentLimitConfigs)
+            .withFeedback(kIntakeFeedbackConfigs)
+            .withMotionMagic(kIntakeMagicConfigs)
+            .withSlot0(kIntakeSlot0Configs);
+    }
+
     public class Coralk {
         public static final int kCoralMotorCANID = 1; //TODO: check real CANID
 
@@ -52,5 +112,4 @@ public class Constants {
             .withMotionMagic(kMagicConfigs)
             .withSlot0(kSlot0Configs);
     }
-    
 }
