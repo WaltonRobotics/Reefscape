a6bf5d7c23e74cad96f3caa009d85bf22a9afc9f
graciexie <xiegrace61@gmail.com>
2025-02-06 10:19:42 -0500

    thsi stuffs completely unfinished but i figured id merge rn

--- src/main/java/frc/robot/autons/WaltAutonFactory.java
+++ src/main/java/frc/robot/autons/WaltAutonFactory.java
@@ -7,137 +7,95 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
+import choreo.trajectory.Trajectory;
 import edu.wpi.first.math.Pair;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.autons.TrajsAndLocs.HPReefPair;
 import frc.robot.autons.TrajsAndLocs.HPStation;
+import frc.robot.autons.TrajsAndLocs.ReefHPPair;
 import frc.robot.autons.TrajsAndLocs.ReefLocs;
-import frc.robot.subsystems.Coral;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Superstructure;
 import frc.robot.subsystems.Swerve;
+import frc.util.Elastic;
 import frc.robot.subsystems.Elevator.EleHeights;
-import frc.robot.subsystems.Superstructure.CoralState;
+import frc.robot.autons.TrajsAndLocs.Trajectories;
 
 public class WaltAutonFactory {
-    private final AutoFactory m_autoFactory;
-    private TrajsAndLocs.Trajectories m_trajs = new TrajsAndLocs.Trajectories();
-    private SequentialCommandGroup m_cmdSched;
-    final AutoRoutine m_routine;
-    private ArrayList<AutoTrajectory> m_trajList = new ArrayList<AutoTrajectory>();
-    private double m_eleWaitSecs = 1; //dummy num
+    private final AutoFactory m_autonFactory;
+    private final SequentialCommandGroup m_fullCmdSequence;
+    private final AutoRoutine m_routine;
 
-    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+    private final SwerveRequest.SwerveDriveBrake m_brake = new SwerveRequest.SwerveDriveBrake();
 
-    public WaltAutonFactory(AutoFactory autoFactory, Swerve swerve, Elevator ele) {
-        m_trajs.configureTrajectories();
-        m_autoFactory = autoFactory;
-        m_routine = m_autoFactory.newRoutine("auton");
-        m_cmdSched = new SequentialCommandGroup();
+    public WaltAutonFactory(AutoFactory autonFactory, Swerve swerve, Elevator ele) {
+        m_autonFactory = autonFactory;
+        m_routine = m_autonFactory.newRoutine("auton");
+        m_fullCmdSequence = new SequentialCommandGroup();
     }
 
-    public SequentialCommandGroup generateAuton(Superstructure superstructure, Swerve drivetrain, Elevator ele, Coral coral, ReefLocs firstScoreLoc, ArrayList<ReefLocs> scoreLocs, ArrayList<EleHeights> eleHeights, ArrayList<CS> CSLocs) {
-        ReefLocs firstLoc = ReefLocs.getSameLoc(firstScoreLoc);
-        AutoTrajectory firstRTraj = m_routine.trajectory(firstScoreLoc.m_startAndTraj.getSecond());
-        AutoTrajectory firstCSTraj = m_routine.trajectory(m_trajs.m_toCSTrajMap.get(new Pair<ReefLocs, HPStation>(firstLoc, CSLocs.get(0))));
+    class AutonCycle {
+        public final ReefLocs reefLoc;
+        public final EleHeights height;
+        public final HPStation hpStation;
+        public final ReefHPPair reefHPPair;
+        public final HPReefPair hpReefPair;
+        Elastic.Notification reefToHPError = 
+                    new Elastic.Notification(
+                        Elastic.Notification.NotificationLevel.ERROR, 
+                        "Invalid reef to HP AutonCycle", 
+                        "UR AUTON SUX AND HAS AN INVALID REEF-TO-HP YOU DUMB funnel cake"
+                    );
+        Elastic.Notification hpToReefError = 
+                        new Elastic.Notification(
+                            Elastic.Notification.NotificationLevel.ERROR, 
+                            "Invalid HP to reef AutonCycle", 
+                            "UR AUTON SUX AND HAS AN INVALID HP-TO-REEF YOU DUMB funnel cake");
+    
+        public AutonCycle(ReefLocs _reef, EleHeights _height, HPStation _hp) {
+            reefLoc = _reef;
+            height = _height;
+            hpStation = _hp;
+            reefHPPair = new ReefHPPair(reefLoc, hpStation);
+            hpReefPair = new HPReefPair(hpStation, reefLoc);
+        }
 
-        /* iteration 1 */
-        m_cmdSched.addCommands(
-            /* 
-             * scoring the first piece 
-            */
-            firstRTraj.resetOdometry(),
-            Commands.runOnce(() -> superstructure.autonPreload()), //SM -> INTOOK
-            Commands.parallel(
-                firstRTraj.cmd(),
-                Commands.sequence(
-                    Commands.waitSeconds(m_eleWaitSecs), //dummy num
-                    ele.setPosition(eleHeights.get(0)).asProxy() // once it reaches pos, SM -> SCORE_READY
-                    )
-                ),
-            drivetrain.applyRequest(() -> brake).asProxy(),
-            Commands.parallel(
-                Commands.runOnce(() -> superstructure.autonScore()), // SM -> SCORING
-                Commands.waitUntil(() -> superstructure.getCoralState().equals(CoralState.IDLE))
-            ),
+        public boolean isLegit() {
+            if(!Trajectories.ReefToHPTrajs.containsKey(reefHPPair)) {
+                // banks said that i actually shouldnt write code that crashes the entire program, which im lowk sad abt. was looking forward to it.
+                Elastic.sendNotification(reefToHPError);
+                return false;
+            }
+    
+            if(!Trajectories.HPToReefTrajs.containsKey(hpReefPair)) {
+                // rip DEATH AND DESTRUCTION you were such a good concept in my head. fly high.
+                Elastic.sendNotification(hpToReefError);
+                return false;
+            }
+            return true;
+        }
+    }
 
-            /* going to cs */
-            firstCSTraj.resetOdometry(),
-            Commands.parallel(
-                firstCSTraj.cmd(),
-                Commands.parallel(
-                    ele.toHome().asProxy(),
-                    Commands.sequence(
-                        Commands.waitSeconds(m_eleWaitSecs),
-                        ele.toCS().asProxy()
-                    )
-                )
-            ),
-            drivetrain.applyRequest(() -> brake).asProxy(),
-            Commands.parallel(
-                Commands.runOnce(() -> superstructure.autonScore()),
-                Commands.waitUntil(() -> superstructure.getCoralState().equals(CoralState.INTOOK))
+    public Command generateAuton(Swerve drivetrain, Superstructure superstructure, ArrayList<AutonCycle> cycles) { // im assuming that autoncycle will include 1st iteration
+        // iterate AutonCycles and validate all HP->Score and Score->HP pairs
+        for (AutonCycle autonCycle : cycles) {
+            if(!autonCycle.isLegit()) {
+                return Commands.print("that one scene in alice in borderland where karube just sits there contemplatively before he blows up.");
+            }
+        }
+
+        AutoTrajectory firstScoreTraj = m_routine.trajectory(Trajectories.ReefToHPTrajs.get(cycles.get(0).reefHPPair));
+        AutoTrajectory firstLoadTraj = m_routine.trajectory(Trajectories.HPToReefTrajs.get(cycles.get(0).hpReefPair));
+
+        m_routine.active().onTrue(
+            Commands.sequence(
+                firstScoreTraj.resetOdometry(),
+                firstScoreTraj.cmd()
             )
         );
 
-        /* rest of the iterations */
-        for (int i = 0; i < scoreLocs.size(); i++) {
-            m_trajList.add(m_routine.trajectory(m_trajs.m_toRTrajMap.get(new Pair<CS, ScoringLocs>(CSLocs.get(i), scoreLocs.get(i)))));
-
-            if(CSLocs.size() >= i + 2) { // thats right, right? am i tripping; i might be tripping (im rlly smart, i know i know)
-                m_trajList.add(m_routine.trajectory(m_trajs.m_toCSTrajMap.get(new Pair<ScoringLocs, CS>(scoreLocs.get(i), CSLocs.get(i + 1)))));
-            }
-        }
-
-        int heightCounter = 1;
-        for (int i = 0; i < m_trajList.size(); i++) {
-            m_cmdSched.addCommands(
-                m_trajList.get(i).resetOdometry(), // im actually a tad unsure if ur supposed to reset odometry before every path or j the first one. will find out at some pt  
-                Commands.parallel(
-                    m_trajList.get(i).cmd(),
-                    Commands.parallel(
-                        ele.toHome().asProxy(),
-                        Commands.sequence(
-                            Commands.waitSeconds(m_eleWaitSecs),
-                            ele.setPosition(eleHeights.get(heightCounter)).asProxy()
-                        )
-                    )
-                ),
-                drivetrain.applyRequest(() -> brake).asProxy(),
-                Commands.parallel(
-                    Commands.runOnce(() -> superstructure.autonScore()),
-                    Commands.waitUntil(() -> superstructure.getCoralState().equals(CoralState.IDLE))
-                )
-            );
-
-            heightCounter++;
-
-            if(m_trajList.size() >= i + 2) {
-                i++;
-                m_cmdSched.addCommands(
-                    m_trajList.get(i).resetOdometry(),
-                    Commands.parallel(
-                        m_trajList.get(i).cmd(),
-                        Commands.parallel(
-                            ele.toHome().asProxy(),
-                            Commands.sequence(
-                                Commands.waitSeconds(m_eleWaitSecs),
-                                ele.toCS().asProxy()
-                            )
-                        )
-                    ),
-                    drivetrain.applyRequest(() -> brake).asProxy(),
-                    Commands.waitUntil(() -> superstructure.getCoralState().equals(CoralState.INTOOK))
-                );
-            }
-        }
-
-        return m_cmdSched;
-    }
-
-    public AutoRoutine getAuton() {
-        m_routine.active().onTrue(m_cmdSched);
-
-        return m_routine;
+        firstScoreTraj.atTime("eleUp").onTrue()
     }
 }
