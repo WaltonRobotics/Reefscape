7d8dddb31907e43da5dc93adca6d143de8aa8301
sohnshaik <sohan.shaik@waltonrobotics.org>
2025-02-08 15:54:50 -0500

    final stretch started
    started making the reefToHP and hpToReef

--- src/main/java/frc/robot/autons/AutonChooser.java
+++ src/main/java/frc/robot/autons/AutonChooser.java
@@ -3,7 +3,6 @@ package frc.robot.autons;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -18,10 +17,20 @@ public class AutonChooser {
     private static EnumMap<ReefLocs, String> firstScoringMap = new EnumMap<>(TrajsAndLocs.ReefLocs.class);
     private static SendableChooser<ReefLocs> firstScoringChooser = new SendableChooser<ReefLocs>();
     
-    private static Supplier<StartingLocs> startLocChosen = () -> startingPositionChooser.getSelected();
+    private static Supplier<StartingLocs> startLocChosen = () -> startingPositionChooser.getSelected(); //need to constant check if val of startloc changed
 
     private static EnumMap<HPStation, String> hpStationMap = new EnumMap<>(TrajsAndLocs.HPStation.class);
     private static SendableChooser<HPStation> hpStationChooser = new SendableChooser<HPStation>();
+
+    private static Supplier<ReefLocs> reefChosen = () -> firstScoringChooser.getSelected();
+
+    private static EnumMap<ReefLocs, String> hpToReefMap = new EnumMap<>(TrajsAndLocs.ReefLocs.class); 
+    private static SendableChooser<ReefLocs> hpToReefChooser = new SendableChooser<ReefLocs>();
+    
+    private static Supplier<HPStation> hpStationChosen = () -> .getSelected();
+
+    private static EnumMap<HPStation, String> reefToHPMap = new EnumMap<>(TrajsAndLocs.HPStation.class); 
+    private static SendableChooser<HPStation> reefToHPChooser = new SendableChooser<HPStation>();
 
     static{
         SmartDashboard.putData("starting position chooser", startingPositionChooser);   
@@ -42,11 +51,20 @@ public class AutonChooser {
         startingPositionChooser.setDefaultOption("default (mid)", scoringLoc);
     }
 
-    public static void chooseHPStation(HPStation hpstation, String description){
+    public static void setDefaultHPStation(HPStation hpStation){
+        hpStationChooser.setDefaultOption("default(left)", hpStation);
+    }
+
+    public void assign
+
+    public static void assignHPStation(HPStation hpstation, String description){
         hpStationMap.put(hpstation, description);
         hpStationChooser.addOption(description, hpstation);
     }
 
+    /**
+     * depending on certain starting location, displays the optimal path for said starting + scoring path
+     */
     public static void chooseFirstScoring(){
         firstScoringChooser = new SendableChooser<ReefLocs>();
 
@@ -76,5 +94,18 @@ public class AutonChooser {
         SmartDashboard.updateValues(); //FINALLY
 
     }
+
+    public 
+
+    public static void chooseReefToHP(){
+        reefToHPChooser = new SendableChooser<HPStation>();
+
+        if(hpStationChosen.get().equals(TrajsAndLocs.HPStation.HP_RIGHT)){
+            for (int i = 0; i < TrajsAndLocs.Trajectories.ReefToHPTrajs.size(); i++) {
+                
+            }
+        }
+    }
+    
     
 }
