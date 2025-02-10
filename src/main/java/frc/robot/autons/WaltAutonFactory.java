b48ad7626c1bc9beb085c61ddf3a9fd246940c7f
graciexie <xiegrace61@gmail.com>
2025-02-10 17:59:18 -0500

    meow !

--- src/main/java/frc/robot/autons/WaltAutonFactory.java
+++ src/main/java/frc/robot/autons/WaltAutonFactory.java
@@ -40,13 +40,13 @@ public class WaltAutonFactory {
                     new Elastic.Notification(
                         Elastic.Notification.NotificationLevel.ERROR, 
                         "Invalid reef to HP AutonCycle", 
-                        "UR AUTON is unworking AND HAS AN INVALID REEF-TO-HP YOU rlly smart funnel cake"
+                        "INVALID REEF-TO-HP: if this auton was a cat, you just killed it."
                     );
         Elastic.Notification hpToReefError = 
                         new Elastic.Notification(
                             Elastic.Notification.NotificationLevel.ERROR, 
                             "Invalid HP to reef AutonCycle", 
-                            "UR AUTON is unworking AND HAS AN INVALID HP-TO-REEF YOU rlly smart funnel cake");
+                            "INVALID HP-TO-REEF: if this auton was a cat, you just killed it.");
     
         public AutonCycle(ReefLocs _reef, EleHeight _height, HPStation _hp) {
             reefLoc = _reef;
