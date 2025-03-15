49e110085bae763268ba7a7c6fe05b7c4a0c9013
graciexie <xiegrace61@gmail.com>
2025-03-15 10:13:49 -0400

    new treads wahoo

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -83,7 +83,7 @@ public class TunerConstants {
     private static final double kSteerGearRatio = 21.428571428571427;
     private static final double kWheelIDInches = 3.150; // caliper measured
     private static final double kWheelWallAndTreadInches = 0.330; // caliper measured
-    public static final double kWheelDiameterInches = kWheelIDInches + (kWheelWallAndTreadInches * 2);
+    public static final double kWheelDiameterInches = 3.86;
     public static final Distance kWheelRadius = Inches.of(kWheelDiameterInches / 2);
 
     private static final boolean kInvertLeftSide = false;
