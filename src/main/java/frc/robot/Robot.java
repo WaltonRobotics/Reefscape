442f51b1fa797f3a3a595b93861d5533b571e2fc
graciexie <xiegrace61@gmail.com>
2025-03-21 09:44:52 -0400

    changed first score to L2

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -520,7 +520,7 @@ public class Robot extends TimedRobot {
           drivetrain,
           StartingLocs.RIGHT, 
           new ArrayList<>(List.of(REEF_E, REEF_D, REEF_C)), 
-          new ArrayList<>(List.of(EleHeight.L4, EleHeight.L4, EleHeight.L4)), 
+          new ArrayList<>(List.of(EleHeight.L2, EleHeight.L4, EleHeight.L4)), 
           new ArrayList<>(List.of(HPStation.HP_RIGHT, HPStation.HP_RIGHT, HPStation.HP_RIGHT)),
           WaltAutonBuilder.nte_autonRobotPush.getBoolean(false)
         ));
@@ -538,7 +538,7 @@ public class Robot extends TimedRobot {
           drivetrain,
           StartingLocs.LEFT, 
           new ArrayList<>(List.of(REEF_J, REEF_K, REEF_L)), 
-          new ArrayList<>(List.of(EleHeight.L4, EleHeight.L4, EleHeight.L4)), 
+          new ArrayList<>(List.of(EleHeight.L2, EleHeight.L4, EleHeight.L4)), 
           new ArrayList<>(List.of(HPStation.HP_LEFT, HPStation.HP_LEFT, HPStation.HP_LEFT)),
           WaltAutonBuilder.nte_autonRobotPush.getBoolean(false)
         ));
