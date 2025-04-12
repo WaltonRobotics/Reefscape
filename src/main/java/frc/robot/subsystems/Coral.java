d531e56bde5851574a87dc4ab2a311f4e6fd60ea
graciexie <xiegrace61@gmail.com>
2025-04-12 14:59:30 -0400

    Revert "coral cmd"
    This reverts commit 3783aca9cf6d487b1ba62581d9bb473a30d0eb0a.

--- src/main/java/frc/robot/subsystems/Coral.java
+++ src/main/java/frc/robot/subsystems/Coral.java
@@ -87,10 +87,6 @@ public class Coral extends SubsystemBase {
         return setCoralMotorActionCmd(m_scoreSpeed);
     }
 
-    public Command slowScore() {
-        return setCoralMotorActionCmd(-m_slowIntakeSpeed);
-    }
-
     public void runWheelsAlgaeRemoval() {
         setCoralMotorAction(-m_fingerSpeed);
     }
